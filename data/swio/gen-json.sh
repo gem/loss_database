038ff45dd59704926d6b0ac2e804c74747af71ad
Paul Henshaw <paul@openquake.org>
2019-04-11 17:18:43 +0200

    JSON meta-data files for SWIO losses

---
+++ data/swio/gen-json.sh
@@ -0,0 +1,24 @@
+sed -e 's/@COUNTRY@/Madagascar/g' -e 's/@ISO@/mdg/g' -e 's/@PERIL@/Earthquake/g' -e 's/@TH@/eq/g' template.json > mdg-eq.json
+sed -e 's/@COUNTRY@/Madagascar/g' -e 's/@ISO@/mdg/g' -e 's/@PERIL@/Tropical Cyclone/g' -e 's/@TH@/tc/g' template.json > mdg-tc.json
+sed -e 's/@COUNTRY@/Madagascar/g' -e 's/@ISO@/mdg/g' -e 's/@PERIL@/Non-Tropical Cyclone/g' -e 's/@TH@/ntc/g' template.json > mdg-ntc.json
+sed -e 's/@COUNTRY@/Madagascar/g' -e 's/@ISO@/mdg/g' -e 's/@PERIL@/All Perils/g' -e 's/@TH@/ap/g' template.json > mdg-ap.json
+
+sed -e 's/@COUNTRY@/Comoros/g' -e 's/@ISO@/com/g' -e 's/@PERIL@/Earthquake/g' -e 's/@TH@/eq/g' template.json > com-eq.json
+sed -e 's/@COUNTRY@/Comoros/g' -e 's/@ISO@/com/g' -e 's/@PERIL@/Tropical Cyclone/g' -e 's/@TH@/tc/g' template.json > com-tc.json
+sed -e 's/@COUNTRY@/Comoros/g' -e 's/@ISO@/com/g' -e 's/@PERIL@/Non-Tropical Cyclone/g' -e 's/@TH@/ntc/g' template.json > com-ntc.json
+sed -e 's/@COUNTRY@/Comoros/g' -e 's/@ISO@/com/g' -e 's/@PERIL@/All Perils/g' -e 's/@TH@/ap/g' template.json > com-ap.json
+
+sed -e 's/@COUNTRY@/Mauritius/g' -e 's/@ISO@/mus/g' -e 's/@PERIL@/Earthquake/g' -e 's/@TH@/eq/g' template.json > mus-eq.json
+sed -e 's/@COUNTRY@/Mauritius/g' -e 's/@ISO@/mus/g' -e 's/@PERIL@/Tropical Cyclone/g' -e 's/@TH@/tc/g' template.json > mus-tc.json
+sed -e 's/@COUNTRY@/Mauritius/g' -e 's/@ISO@/mus/g' -e 's/@PERIL@/Non-Tropical Cyclone/g' -e 's/@TH@/ntc/g' template.json > mus-ntc.json
+sed -e 's/@COUNTRY@/Mauritius/g' -e 's/@ISO@/mus/g' -e 's/@PERIL@/All Perils/g' -e 's/@TH@/ap/g' template.json > mus-ap.json
+
+sed -e 's/@COUNTRY@/Seychelles/g' -e 's/@ISO@/syc/g' -e 's/@PERIL@/Earthquake/g' -e 's/@TH@/eq/g' template.json > syc-eq.json
+sed -e 's/@COUNTRY@/Seychelles/g' -e 's/@ISO@/syc/g' -e 's/@PERIL@/Tropical Cyclone/g' -e 's/@TH@/tc/g' template.json > syc-tc.json
+sed -e 's/@COUNTRY@/Seychelles/g' -e 's/@ISO@/syc/g' -e 's/@PERIL@/Non-Tropical Cyclone/g' -e 's/@TH@/ntc/g' template.json > syc-ntc.json
+sed -e 's/@COUNTRY@/Seychelles/g' -e 's/@ISO@/syc/g' -e 's/@PERIL@/All Perils/g' -e 's/@TH@/ap/g' template.json > syc-ap.json
+
+sed -e 's/@COUNTRY@/Zanzibar/g' -e 's/@ISO@/zan/g' -e 's/@PERIL@/Earthquake/g' -e 's/@TH@/eq/g' template.json > zan-eq.json
+sed -e 's/@COUNTRY@/Zanzibar/g' -e 's/@ISO@/zan/g' -e 's/@PERIL@/Tropical Cyclone/g' -e 's/@TH@/tc/g' template.json > zan-tc.json
+sed -e 's/@COUNTRY@/Zanzibar/g' -e 's/@ISO@/zan/g' -e 's/@PERIL@/Non-Tropical Cyclone/g' -e 's/@TH@/ntc/g' template.json > zan-ntc.json
+sed -e 's/@COUNTRY@/Zanzibar/g' -e 's/@ISO@/zan/g' -e 's/@PERIL@/All Perils/g' -e 's/@TH@/ap/g' template.json > zan-ap.json
---
+++ data/swio/import-loss-shp.sh
@@ -0,0 +1,11 @@
+if [ $# -lt 1 ] 
+then
+	echo "Usage $0 <shapefile>"
+	exit 1
+fi
+
+filename=$1
+table_name=$(basename $filename .shp)
+
+echo "shp2pgsql -c -W UTF-8 -s 4326 $filename temp.$table_name  > $table_name.sql"
+shp2pgsql -c -W UTF-8 -s 4326 $filename temp.$table_name  > $table_name.sql

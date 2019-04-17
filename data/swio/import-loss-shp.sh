5b9e3af1b1bdabce6f2cbfa5dbb3d3b6a9342df3
Paul Henshaw <paul@openquake.org>
2019-04-17 17:45:42 +0200

    Quotes around filename

--- data/swio/import-loss-shp.sh
+++ data/swio/import-loss-shp.sh
@@ -8,4 +8,4 @@ filename=$1
 table_name=$(basename $filename .shp)
 
 echo "shp2pgsql -c -W UTF-8 -s 4326 $filename temp.$table_name  > $table_name.sql"
-shp2pgsql -c -W UTF-8 -s 4326 $filename temp.$table_name  > $table_name.sql
+shp2pgsql -c -W UTF-8 -s 4326 "$filename" temp.$table_name  > $table_name.sql

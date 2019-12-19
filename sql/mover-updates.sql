c2621682bbd69854426f0fb27b48274963dc321d
Paul Henshaw <paul@openquake.org>
2019-12-19 15:13:12 +0100

    Use TIMESTAMP WITHOUT TIME ZONE for contributed_date - forcing UTC in import/export scripts

--- sql/mover-updates.sql
+++ sql/mover-updates.sql
@@ -1,10 +1,5 @@
 -- SQL statements to be applied to the CF loss database 
 
--- We assume a standard installation using UTC - if you use
--- a custom timezone, consider using a statement similar to the one below
--- a to force a UTC timezone
--- SET TIMEZONE TO 'Etc/UTC'
-
 -- Add a project field
 ALTER TABLE loss.contribution ADD COLUMN IF NOT EXISTS project VARCHAR;
 -- All loss contributions with id<=90 come from the SWIO RAFI project
@@ -12,5 +7,5 @@ UPDATE loss.contribution SET project = 'SWIO RAFI' WHERE loss_model_id<=90;
 
 -- Add a contributed_at timestamp (with time zone)
 ALTER TABLE loss.contribution ADD COLUMN IF NOT EXISTS 
-	contributed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW();
+	contributed_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW();
 UPDATE loss.contribution SET contributed_at = '2019-04-01 00:00:00+0' WHERE loss_model_id<=90;
